4011f1fb12f42d6f4d7fc7df40a076d36d274e41
chromeworld digital <igahfranklyn@gmail.com>
2022-12-20 16:42:09 +0100

    dev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>dev branch</h2>
+      <h2>
+        dev branch Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+        hic tempora nemo aliquid ad, alias nihil minima saepe laboriosam
+        provident soluta delectus aspernatur vel quos, cupiditate numquam
+        adipisci quae laudantium!
+      </h2>
     </div>
   );
 }
